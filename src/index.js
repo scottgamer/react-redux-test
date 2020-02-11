67e85cfa86cbe6cd4d8181ff14bded89eb4e645e
richardmunoz <richard.munoz@crifa.com>
2020-02-11 12:08:35 -0500

    added multiple reducers

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import reducer from './store/reducer';
+
+import counterReducer from './store/reducers/counter';
+import resultsReducer from './store/reducers/result';
+
+const reducer = combineReducers({
+  counterReducer,
+  resultsReducer
+});
 
 const store = createStore(reducer);
 
